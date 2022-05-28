232abefc80b907b3b299426b967968a3868a07fe
SCatella <samuel.catella@gmail.com>
2022-05-28 14:01:56 -0600

    refactoring the current build to separate components properly

--- src/App.js
+++ src/App.js
@@ -1,41 +1,56 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
+import CardList from './components/card-list/card-list.component'
 import './App.css';
 
 class App extends Component {
+
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-          {
-          name: 'Linda',
-          id: 'asdfarfa',
-        },
-        {
-          name: 'Frank',
-          id: 'gfukfhjk',
-        },
-        {
-          name: 'Jackie',
-          id: 'asdtra',
-        }
-      ]
-      
-    }
+      monsters: [],
+      searchField: '',
+    };
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          }
+        )
+      );
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
     return (
       <div className='App'>
-        {
-          this.state.monsters.map((monster) => {
-            return <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>;
-          })
-        }
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={onSearchChange}
+        />
+        <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
