7d3d3d7860c605bf0d19ff3a749d07e15be0dbcc
SCatella <samuel.catella@gmail.com>
2022-05-29 10:37:08 -0600

    built out card, card-list, and search-box components. Refactoring the code from App into said components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 
+import SearchBox from './components/search-box/search-box.component'
 import CardList from './components/card-list/card-list.component'
 import './App.css';
 
@@ -44,11 +45,11 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <input
-          className='search-box'
-          type='search'
+        <h1 className='app-title'>MonsterDex</h1>
+        <SearchBox
+          className='monsters-search-box'
+          onChangeHandler={onSearchChange}
           placeholder='search monsters'
-          onChange={onSearchChange}
         />
         <CardList monsters={filteredMonsters}/>
       </div>
