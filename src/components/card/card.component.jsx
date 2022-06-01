4853af573dcac04133b3f93696b673e08f24ca3e
SCatella <samuel.catella@gmail.com>
2022-05-31 20:26:05 -0600

    switched card-list from grid to flex to respond the way I wanted.

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -7,7 +7,7 @@ class Card extends Component {
     const { id, name, email } = this.props.monster;
     return (
       <div className='card-container' key={id}>
-        <img alt={`monster ${name}`} src={`https://robohash.org/${id}&size=180x180`} />
+        <img className='monster-img' alt={`monster ${name}`} src={`https://robohash.org/${id}`} />
         <h2>{name}</h2>
         <p>{email}</p>
       </div>
